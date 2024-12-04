5d33331890cf39264a734abb9ae021414cb764dc
NezraDev <nezraprogrammer@gmail.com>
2024-12-04 09:43:17 +0800

    feat : add static files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './TECHVENTORY/src/main/resources/templates/**/*.{html,js}', 
-    './TECHVENTORY/src/main/resources/static/**/*.html',         
+    './src/main/resources/templates/**/*.{html,js}', 
+    './src/main/resources/static/**/*.html',         
     './src/**/*.{js,ts,jsx,tsx}'   
   ],
   theme: {

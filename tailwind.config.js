09d937b5dfcfde012b9931e4360bf2c74c64ef59
NezraDev <nezraprogrammer@gmail.com>
2024-12-04 09:31:42 +0800

    feat : add content on tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './TECHVENTORY/src/main/resources/templates/**/*.{html,js}', 
+    './TECHVENTORY/src/main/resources/static/**/*.html',         
+    './src/**/*.{js,ts,jsx,tsx}'   
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

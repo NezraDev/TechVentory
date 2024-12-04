624124423aa9a2201cec43fe0a1d7cdcc13cf277
NezraDev <nezraprogrammer@gmail.com>
2024-12-04 09:27:18 +0800

    feat : added files for developing

---
+++ src/test/java/TechVentory/TechVentory/TechVentoryApplicationTests.java
@@ -0,0 +1,13 @@
+package TechVentory.TechVentory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechVentoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
